c6428c4953f6b7f81c571ab3b37a0732356ff145
ebooklover0409 <elisabethwilliams49@gmail.com>
2026-01-26 14:45:43 -0700

    new file:   ChapterFour/FlowofExecution modified:   ChapterTwo/Date.java

--- ChapterTwo/Date.java
+++ ChapterTwo/Date.java
@@ -2,14 +2,24 @@ package ChapterTwo;
 
 public class Date {
     
+    public static void printAmerican(String day, String month, int year, int date) {
+        System.out.print("American format: "); 
+        System.out.print(day + ", " + month + " " + date + ", " + year); 
+    }
+
+    public static void printEuropean(String day, int date, String month, int year) { 
+        System.out.print("\nEuropean format: "); 
+        System.out.print(day + " " + date + " " + month + " " + year);
+    }
     public static void main(String[] args) {
         String day  = "Thursday";
         String month = "July"; 
         int date = 18; 
-        int year = 2019;
-        System.out.print("American format: "); 
-        System.out.print(day + ", " + month + " " + date + ", " + year); 
-        System.out.print("\nEuropean format: "); 
-        System.out.print(day + " " + date + " " + month + " " + year);
+        int year = 2026; 
+        printAmerican(day, month, year, date); 
+        printEuropean(day, date, month, year);
     }
-}
+    
+    }
+
+
